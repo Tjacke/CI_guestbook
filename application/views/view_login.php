f66fe015974da0652e754fbbecae123ebf8d1ccc
Hakan Aslin <tjacke@hotmail.com>
2013-04-23 11:48:58 +0200

    Login + Guestbok

---
+++ application/views/view_login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $title; ?></title>
+</head>
+<div id="container">
+    <h1>Det här är vi!</h1>
+    
+    <? echo $menu; ?>
+   <p>Content...</p>
+</div>
+<? echo $footer; ?>
+</body>
+</html>
